b4caab0d183aa9d5ac27206d4e43487773602abd
Mercy Ikoyi Godwin <ikoyi850@gmail.com>
2022-09-13 21:42:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,19 @@
-#ifndif
-#define
+#ifndef PUTCHAR_INCLUDED
+#define PUTCHAR_INCLUDED
 
 int _putchar(char);
-void print_alphabet(void
-#endif
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _is alpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_baur(void);
+void times_table(void);
+int add (int a,int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
+
+#endiif
