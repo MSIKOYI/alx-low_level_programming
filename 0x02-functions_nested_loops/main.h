918de222b76455ad0e5b7f5b3a1fe353e09d3361
Mercy Ikoyi Godwin <ikoyi850@gmail.com>
2022-09-14 15:16:30 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifndef HOLBERTON_H_ INCLUDED
+#ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
@@ -9,9 +9,9 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack_baur(void);
+void jack_bauer(void);
 void times_table(void);
-int add (int a,int b);
+int add (int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
 void num(int r, int n);
