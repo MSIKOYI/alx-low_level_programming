3297af62aef2d237c58fffaa3d3788fd8818850c
Mercy Ikoyi Godwin <ikoyi850@gmail.com>
2022-09-15 08:18:30 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
- * ng from 00:00 to 23:59, min loop counts mins, while hour loop counts hours 
+ * ng from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
  * and reset minutes
  * Return: 0
  */
@@ -31,7 +31,6 @@ void jack_bauer(void)
 	minutes++;
 
 	_putchar('\n');
-	
 	}
 
 	hours++;
