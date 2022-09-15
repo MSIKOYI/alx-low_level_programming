e72ea51855f2888167ae0c42514002c9dcf06cb8
Mercy Ikoyi Godwin <ikoyi850@gmail.com>
2022-09-15 12:05:29 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinit loop
  * Return: 0
  */
 
 int main(void)
+
 {
 	int i;
+
 	printf("infinite loop incoming :(\n");
 
 	i = 0;
 
 	while (i < 10)
+
 	{
 		putchar(i);
 	}
