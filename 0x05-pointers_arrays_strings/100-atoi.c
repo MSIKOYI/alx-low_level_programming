accb09149e6fb9948bb89021bb73e0d544dbcab3
Mercy Ikoyi Godwin <ikoyi850@gmail.com>
2022-09-19 13:24:05 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,6 +7,34 @@
  */
 int _atoi(char *s)
 {
+	int i, j, p;
+	unsigned int n;
+	char c;
 
-	return (0);
+	i = 0;
+	while (*(s + i))
+	{
+		i++;
+	}
+	i--;
+	n = 0;
+	p = 1;
+	for (j = 0; j <= i; j++)
+	{
+		c = *(s + j);
+		if (c == '-')
+		{
+			p *= -1;
+		}
+		else if (c >= '0' && c <= '9')
+		{
+			n = n * 10 + (c - '0');
+		}
+		else if (n > 0)
+		{
+			break;
+		}
+	}
+
+	return (p * n);
 }
