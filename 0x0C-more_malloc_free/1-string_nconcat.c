1abfe116d60ac205489ba942741640fc089f1169
Mercy Ikoyi Godwin <ikoyi850@gmail.com>
2022-10-07 02:31:46 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,48 +1,53 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
+
 /**
- * string_nconcat - concatenates 2 strings into a new string,
- * taking the first n from string 2. If n>strlen(s2),
- * use whole string s2.
+ * string_nconcat - Concatenates two strings
+ * @s1: The first string
+ * @s2: The second string
+ * @n: The number of bytes of s2 to copy
  *
- * @s1: first string
- * @s2: second string
- * @n: number of characters to take from second string
- *
- * Return: new string, or null on failure
+ * Return: A pointer to a new string containing s1
+ * and n bytes of s2. Or NULL if program fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1, len2, i;
-	char *ptr, *ret;
+	char *str;
+	unsigned int i, j, len1 = 0, len2 = 0;
 
-	if (!s1)
+	if (s1 == NULL)
 		s1 = "";
-	if (!s2)
+
+	if (s2 == NULL)
 		s2 = "";
 
-	for (len1 = 0, ptr = s1; *ptr; ptr++)
+	while (s1[len1])
 		len1++;
-	for (len2 = 0, ptr = s2; *ptr; ptr++)
+
+	while (s2[len2])
 		len2++;
-	if (n > len2)
+
+	if (n >= len2)
 		n = len2;
 
-		ret = malloc((len1 + n + 1) * sizeof(char));
-		if (!ret)
-			return (0);
+	/*str = malloc((sizeof(s1) + n) * sizeof(char));*/
+	str = malloc(sizeof(char) * (len1 + n) + 1);
 
-		ptr = ret;
-		while (*s1)
-			*ptr++ = *s1++;
+	if (str == NULL)
+	{
+		return (NULL);
+	}
 
-		i = 0;
-		while (i < n)
+	for (i = 0; s1[i]; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; j < n; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
 
-		{
-			*ptr++ = s2[i++];
-		}
-
-		*ptr = 0;
+	str[i] = '\0';
+	return (str);
 }
